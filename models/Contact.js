215e79ef4cf4650603693bd12c291070c4672b0b
NadiaOl <115547708+NadiaOl@users.noreply.github.com>
2026-04-25 20:15:28 +0300

    update

--- models/Contact.js
+++ models/Contact.js
@@ -16,7 +16,6 @@ const contactSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true,
     trim: true, // Убирает случайные пробелы
     lowercase: true, // Преобразует "User@Mail.Com" в "user@mail.com"
     match: [/^\S+@\S+\.\S+$/, "Пожалуйста, введите корректный email"], // Базовая проверка регуляркой
@@ -24,9 +23,6 @@ const contactSchema = new mongoose.Schema({
   phone: {
     type: String,
     trim: true,
-    // Минимальная и максимальная длина, чтобы не сохраняли пустые или слишком длинные строки
-    minlength: [10, "Номер телефона слишком короткий"],
-    maxlength: [20, "Номер телефона слишком длинный"],
   },
 });
 
